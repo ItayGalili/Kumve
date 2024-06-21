8fc47b22058c2c16e8878160f3376bfc3b7a71b4
ItayGalili <131757883+ItayGalili@users.noreply.github.com>
2024-06-21 14:45:14 +0300

    image lode

--- app/src/main/java/com/example/mykumve/TravelManager.kt
+++ app/src/main/java/com/example/mykumve/TravelManager.kt
@@ -1,9 +1,13 @@
 package com.example.mykumve
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mykumve.databinding.TravelManagerViewBinding
@@ -13,6 +17,13 @@ class TravelManager : Fragment() {
     private var _binding : TravelManagerViewBinding? = null
     private val binding get() = _binding!!
 
+    private var imageUri: Uri? = null
+    val pickImageLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) {
+            binding.tripImage.setImageURI(it)
+            requireActivity().contentResolver.takePersistableUriPermission(it!!, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            imageUri = it
+        }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
@@ -33,6 +44,9 @@ class TravelManager : Fragment() {
             findNavController().navigate(R.id.action_travelManager_to_mainScreenManager)
         }
 
+        binding.tripImage.setOnClickListener {
+            pickImageLauncher.launch(arrayOf("image/*"))
+        }
         return binding.root
     }
 
