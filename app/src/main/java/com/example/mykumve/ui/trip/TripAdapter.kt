0f91db8685a39dc429055ec07fe8e3bf0eb96c97
danielenikandil <daniel.eni@gmail.com>
2024-06-22 14:16:01 +0300

    add dummy test to trip card

--- app/src/main/java/com/example/mykumve/ui/trip/TripAdapter.kt
+++ app/src/main/java/com/example/mykumve/ui/trip/TripAdapter.kt
@@ -6,15 +6,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mykumve.data.model.Trip
 import com.example.mykumve.databinding.TravelCardBinding
 import com.example.mykumve.ui.viewmodel.TripViewModel
+import com.example.mykumve.util.Converters
+import com.example.mykumve.util.Utility.toFormattedString
 
 class TripAdapter(var trips: List<Trip>, private val viewModel: TripViewModel) : RecyclerView.Adapter<TripAdapter.TripViewHolder>() {
 
     class TripViewHolder(private val binding: TravelCardBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(trip: Trip, viewModel: TripViewModel) {
             binding.tripTitle.text = trip.title
-            binding.areaCard.text = trip.gatherPlace
-            binding.dateCard.text = trip.gatherTime.toString()
-            binding.levelCard.text = viewModel.getTripInfoByTripId(trip.id).toString()
+            binding.areaCard.text = "Dummy area"
+            binding.dateCard.text = Converters().toDate(trip.gatherTime.toString().toLong())?.toFormattedString()
+            binding.levelCard.text = "Dummy difficulty"
             // TODO: load the image
         }
     }
