3b0a2b6357b94ecd797a9935ec801213db37642b
ItayGalili <131757883+ItayGalili@users.noreply.github.com>
2024-06-30 19:55:07 +0300

    רשימת

--- app/src/main/java/com/example/mykumve/ui/trip/PartnerListFragment.kt
+++ app/src/main/java/com/example/mykumve/ui/trip/PartnerListFragment.kt
@@ -1,171 +1,104 @@
 package com.example.mykumve.ui.trip
+import androidx.recyclerview.widget.RecyclerView
 
-import android.content.SharedPreferences
-import android.graphics.Color
-import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.example.mykumve.R
-import com.example.mykumve.data.data_classes.Equipment
-import com.example.mykumve.databinding.EquipmentCardBinding
+import android.widget.ArrayAdapter
+import android.widget.AdapterView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mykumve.databinding.FragmentPartnerListBinding
-import com.example.mykumve.ui.viewmodel.SharedTripViewModel
-import com.example.mykumve.util.UserManager
+import com.example.mykumve.data.model.User
+import com.example.mykumve.ui.trip.adapter.PartnerListAdapter
+import com.example.mykumve.UserViewModel
 
-class PartnerListFragment:Fragment() {
+class PartnerListFragment : Fragment() {
 
-    private var _binding: FragmentPartnerListBinding? = null
-    private val binding get() = _binding!!
-    //private lateinit var adapter: PartnerAdapter
-    //private lateinit var sharedPreferences: SharedPreferences
-    //private val sharedTripViewModel: SharedTripViewModel by activityViewModels()
+    private lateinit var binding: FragmentPartnerListBinding
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var adapter: PartnerListAdapter
 
+    private var selectedUser: User? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPartnerListBinding.inflate(inflater, container, false)
-        /*
-        sharedPreferences =
-            requireContext().getSharedPreferences("equipment_prefs", Context.MODE_PRIVATE)*/
+        binding = FragmentPartnerListBinding.inflate(inflater, container, false)
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        setupRecyclerView()
+        setupSpinner()
+        setupAddPartnerButton()
+        setupClosePartnerButton()
+
         return binding.root
     }
 
+    private fun setupRecyclerView() {
+        adapter = PartnerListAdapter()
+        binding.recyclerView2.adapter = adapter
+        binding.recyclerView2.layoutManager = LinearLayoutManager(requireContext())
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
 
-        //binding.recyclerView2.layoutManager = LinearLayoutManager(requireContext())
-        //adapter = PartnerAdapter(loadData())
-        //binding.recyclerView2.adapter = adapter
-
-        //val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(adapter))
-        //itemTouchHelper.attachToRecyclerView(binding.recyclerView2)
-
-        binding.addPartnerBtn.setOnClickListener {
-            //addNewEquipment()
-        }
-
-        binding.closePartnerBtn.setOnClickListener {
-            //saveData()
-//            sharedTripViewModel.updateEquipment(adapter.getEquipmentList())  // Update equipment list in the view model
-            findNavController().navigate(R.id.action_equipmentFragment_to_travelManager) //todo bug- should return to the page you came from
-        }
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val user = adapter.getUserAtPosition(position)
+                adapter.removeUserAtPosition(position)
+                userViewModel.deleteUser(user)
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView2)
     }
 
-    /*
-    private fun addNewEquipment() {
+    private fun setupSpinner() {
+        userViewModel.getAllUsers()?.observe(viewLifecycleOwner, Observer { users ->
+            val userNames = users.map { it.firstName }
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, userNames)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinner.adapter = adapter
 
-        val newEquipment = Equipment("New Equipment ${adapter.itemCount + 1}", false,  null) //todo
-        adapter.addEquipment(newEquipment)
-    }
-
-    private fun saveData() {
-        sharedTripViewModel.updateEquipment(adapter.getEquipmentList())
-    }
-
-    private fun loadData(): MutableList<Equipment> {
-        return sharedTripViewModel.equipmentList.value?.toMutableList() ?: mutableListOf()  // Changed line
-    }
-
-    */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
-/*
-class PartnerAdapter(private val PartnerList: MutableList<Partner>) :
-    RecyclerView.Adapter<PartnerAdapter.PartnerViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EquipmentViewHolder {
-        val binding =
-            EquipmentCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return EquipmentViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: EquipmentViewHolder, position: Int) {
-        val equipment = equipmentList[position]
-        holder.bind(equipment, this)
-    }
-
-    override fun getItemCount() = equipmentList.size
-
-    fun addEquipment(equipment: Equipment) {
-        equipmentList.add(equipment)
-        notifyItemInserted(equipmentList.size - 1)
-    }
-
-    fun removeEquipment(position: Int) {
-        equipmentList.removeAt(position)
-        notifyItemRemoved(position)
-    }
-
-    fun getEquipmentList(): MutableList<Equipment> {
-        return equipmentList
-    }
-
-    fun updateEquipment(position: Int, updatedEquipment: Equipment) {
-        equipmentList[position] = updatedEquipment
-        notifyItemChanged(position)
-    }
-
-    class PartnerViewHolder(private val binding: EquipmentCardBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(equipment: Equipment, adapter: EquipmentAdapter) {
-            UserManager.getUser()?.let {
-                val userId = it.id
-                val userFullName = if (equipment.done) "${it.firstName} ${it.surname}" else ""
-                binding.equipmentName.text = Editable.Factory.getInstance().newEditable(equipment.name)
-                binding.nameRes.text = userFullName
-                binding.equipmentResponsibility.isChecked = equipment.done
-
-                binding.equipmentResponsibility.setOnClickListener {
-                    val newDoneStatus = binding.equipmentResponsibility.isChecked
-
-                    if (newDoneStatus) {
-                        binding.equipmentResponsibility.setBackgroundColor(Color.parseColor("#8BC34A"))
-                        binding.equipmentName.setBackgroundColor(Color.parseColor("#8BC34A"))
-                        binding.nameRes.setBackgroundColor(Color.parseColor("#8BC34A"))
-                        binding.nameRes.text = userFullName
-                    } else {
-                        binding.equipmentResponsibility.setBackgroundColor(Color.parseColor("#DE6A6D"))
-                        binding.equipmentName.setBackgroundColor(Color.parseColor("#DE6A6D"))
-                        binding.nameRes.setBackgroundColor(Color.parseColor("#DE6A6D"))
-                        binding.nameRes.text = ""
-                    }
-                    var updatedEquipment = equipment.copy(binding.equipmentName.text.toString(), newDoneStatus, if (newDoneStatus) userId else null)
-                    adapter.updateEquipment(adapterPosition, updatedEquipment)
+            binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    selectedUser = users[position]
                 }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    selectedUser = null
+                }
+            }
+        })
+    }
+
+    private fun setupAddPartnerButton() {
+        binding.addPartner.setOnClickListener {
+            selectedUser?.let {
+                adapter.addUser(it)
             }
         }
     }
-}
-*/
 
-/*
-class SwipeToDeleteCallback(private val adapter: EquipmentAdapter) :
-    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+    private fun setupClosePartnerButton() {
+        binding.closePartnerBtn.setOnClickListener {
+            // שמירת הנתונים שנבחרו
+            val selectedUsers = adapter.getAllUsers()
+            userViewModel.saveSelectedUsers(selectedUsers)
 
-    override fun onMove(
-        recyclerView: RecyclerView,
-        viewHolder: RecyclerView.ViewHolder,
-        target: RecyclerView.ViewHolder
-    ): Boolean {
-        return false
-    }
-
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        val position = viewHolder.adapterPosition
-        adapter.removeEquipment(position)
+            // סגירת הפרגמנט
+            parentFragmentManager.popBackStack()
+        }
     }
 }
-*/
