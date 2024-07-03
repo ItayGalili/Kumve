48b0f96374d8b1be275c13204d6252cfba98537b
danielenikandil <daniel.eni@gmail.com>
2024-07-03 18:36:01 +0300

    redundant import

--- app/src/main/java/com/example/mykumve/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/mykumve/ui/map/MapFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mykumve.R
 import android.graphics.Color
-import android.util.Log
 import android.widget.ImageButton
 import android.widget.PopupMenu
 import android.widget.Toast
