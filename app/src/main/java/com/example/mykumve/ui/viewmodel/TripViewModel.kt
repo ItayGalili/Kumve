f0749862239b15d857eb99de22a957d49ee6862d
ItayGalili <131757883+ItayGalili@users.noreply.github.com>
2024-07-04 01:02:24 +0300

    Connecting conflicts

--- app/src/main/java/com/example/mykumve/ui/viewmodel/TripViewModel.kt
+++ app/src/main/java/com/example/mykumve/ui/viewmodel/TripViewModel.kt
@@ -75,7 +75,7 @@ class TripViewModel(
 
     fun updateTripInfo(tripInfo: TripInfo) {
         viewModelScope.launch {
-            val trip = tripRepository.getTripById(tripInfo.tripId)?.value ?: throw Exception("Trip not found")
+            val trip = tripRepository.getTripById(tripInfo.id)?.value ?: throw Exception("Trip not found")
             trip.tripInfoId = tripInfo.id
             tripRepository.updateTrip(trip)
             tripInfoRepository.updateTripInfo(tripInfo)
