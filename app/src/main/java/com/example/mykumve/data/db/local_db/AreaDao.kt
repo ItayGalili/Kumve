24bc169e6135c31d6c6c0fdcb2383ebbcc138b16
danielenikandil <daniel.eni@gmail.com>
2024-07-17 16:09:32 +0300

    remove unused imports

--- app/src/main/java/com/example/mykumve/data/db/local_db/AreaDao.kt
+++ app/src/main/java/com/example/mykumve/data/db/local_db/AreaDao.kt
@@ -1,17 +1,10 @@
 package com.example.mykumve.data.db.local_db
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import com.example.mykumve.data.model.Area
 import com.example.mykumve.data.model.SubArea
-import com.example.mykumve.data.model.Trip
 
-import com.example.mykumve.util.DifficultyLevel
 @Dao
 interface AreaDao {
     @Query("SELECT * FROM areas")
