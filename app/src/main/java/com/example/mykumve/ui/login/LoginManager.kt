b5eca01f277821d801a86ba53953e74ffee47399
danielenikandil <daniel.eni@gmail.com>
2024-07-17 14:03:41 +0300

    log existing users

--- app/src/main/java/com/example/mykumve/ui/login/LoginManager.kt
+++ app/src/main/java/com/example/mykumve/ui/login/LoginManager.kt
@@ -15,10 +15,12 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.mykumve.R
 import com.example.mykumve.databinding.LoginBinding
+import com.example.mykumve.ui.main.MainActivity.Companion.DEBUG_MODE
 import com.example.mykumve.ui.viewmodel.UserViewModel
 import com.example.mykumve.util.EncryptionUtils
 import com.example.mykumve.util.UserManager
 import com.example.mykumve.util.Result
+import com.example.mykumve.util.UserUtils.getFullName
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -69,7 +71,31 @@ class LoginManager : Fragment() {
             findNavController().navigate(R.id.action_loginManager_to_registerManager)
         }
 
+        if (DEBUG_MODE) {
+            logUsers()
+        }
         return binding.root
+    }
+
+    private fun logUsers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                userViewModel.fetchAllUsers()
+                userViewModel.allUsers.collectLatest { users ->
+                    Log.d(TAG, "Found ${users.size} users.")
+                    users.forEach { user ->
+                        val userInfo = """
+                                        User number ${user.id}:
+                                        Name: ${getFullName(user)}
+                                        Phone: ${user.phone}
+                                        Email: ${user.email}
+                                    """.trimIndent()
+                        Log.d(TAG, userInfo)
+                    }
+                }
+
+            }
+        }
     }
 
     private fun loginUser(email: String, password: String, callback: (Result) -> Unit) {
