3ccb84b3b48f0345133abd4e4242512a3b7d46cb
danielenikandil <daniel.eni@gmail.com>
2024-07-20 18:18:41 +0300

    minor changes

--- app/src/main/java/il/co/erg/mykumve/ui/register/RegisterManager.kt
+++ app/src/main/java/il/co/erg/mykumve/ui/register/RegisterManager.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
@@ -23,7 +22,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
-class RegisterManager : Fragment(), CoroutineScope {
+class   RegisterManager : Fragment(), CoroutineScope {
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO
